2558cb5225e8853584300afa65b4e6eb70e05831
CYAHUB <skysassky@gmail.com>
2021-10-14 11:40:17 +0300

    1.5 Nädala ajakulu

---
+++ Google 1/1.5 Nadala ajakulu.py
@@ -0,0 +1,7 @@
+#1.5 Nädala ajakulu
+Aine = int(input("Sisestage ainepunktide arv : "))
+Nädal = int(input("Sistage nädalate arv : "))
+ajakuku = 26
+wat = Aine * ajakuku
+vastus = wat / Nädal
+print(round(vastus))

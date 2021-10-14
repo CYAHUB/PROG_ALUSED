418c23089d567d9b5bbfc77d102810f557903e12
CYAHUB <skysassky@gmail.com>
2021-10-14 10:52:30 +0300

    1.3 Astendamine

---
+++ Google 1/1.3 Astendamine.py
@@ -0,0 +1,6 @@
+#google
+astendaja = int(input("Sisestage astendaja: "))
+astme_alus = int(input("Sisestage astme alus: "))
+astmevastus = astendaja ** astme_alus
+print("Vastus on : " + str(astmevastus))
+

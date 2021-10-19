eb785b7ac98157b39b24694032cfdfd7caff1091
CYAHUB <skysassky@gmail.com>
2021-10-19 09:48:52 +0300

    2.1 Jäätumine

---
+++ Google 2/2.1 Jäätumine.py
@@ -0,0 +1,7 @@
+õhutemperatuur = float(input("Sisestage õhutemperatuur : "))
+if õhutemperatuur < 4:
+    print("")
+    print("On jäätumise oht!")
+else:
+    print("")
+    print("Ei ole jäätumise ohtu")

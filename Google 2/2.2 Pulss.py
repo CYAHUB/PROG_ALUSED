c61203eecba37a1827df9585f990714ed613bcd7
CYAHUB <skysassky@gmail.com>
2021-10-19 11:04:34 +0300

    2.2 Pulss

---
+++ Google 2/2.2 Pulss.py
@@ -0,0 +1,21 @@
+vanus = int(input("Siseta enda vanus: "))
+sugu = input("Sisetage oma sugu: ")
+if sugu == "M" or sugu == "m":
+    max_pulsi_sagedus = 220 - vanus
+if sugu == "N" or sugu == "n":
+    max_pulsi_sagedus = 206 - vanus * 0.88
+treening = int(input("Siseta treeningu tüüp: "))
+if treening == 1:
+    min_puls = max_pulsi_sagedus * 0.5
+    max_puls = max_pulsi_sagedus * 0.7
+elif treening == 2:
+    min_puls = max_pulsi_sagedus * 0.7
+    max_puls = max_pulsi_sagedus * 0.8
+elif treening == 3:
+    min_puls = max_pulsi_sagedus * 0.8
+    max_puls = max_pulsi_sagedus * 0.87
+#NO
+min_puls = round(min_puls)
+max_puls = round(max_puls)
+print("Pulsisagedus peab olema vahemikus " + str(min_puls) + " kuni " + str(max_puls) + " vahel.")
+
